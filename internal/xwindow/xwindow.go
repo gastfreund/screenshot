72991ac272aef7f8285cc3726390c2c8dc5f8c75
DBX12 <dbx12@web.de>
2023-03-20 16:25:07 +0100

    Remove shm branch

--- internal/xwindow/xwindow.go
+++ internal/xwindow/xwindow.go
@@ -6,9 +6,7 @@ import (
 	"image/color"
 
 	"github.com/gastfreund/screenshot/internal/util"
-	"github.com/gen2brain/shm"
 	"github.com/jezek/xgb"
-	mshm "github.com/jezek/xgb/shm"
 	"github.com/jezek/xgb/xinerama"
 	"github.com/jezek/xgb/xproto"
 )
@@ -41,12 +39,6 @@ func Capture(x, y, width, height int) (img *image.RGBA, e error) {
 	x0 := int(primary.XOrg)
 	y0 := int(primary.YOrg)
 
-	useShm := true
-	err = mshm.Init(c)
-	if err != nil {
-		useShm = false
-	}
-
 	screen := xproto.Setup(c).DefaultScreen(c)
 	wholeScreenBounds := image.Rect(0, 0, int(screen.WidthInPixels), int(screen.HeightInPixels))
 	targetBounds := image.Rect(x+x0, y+y0, x+x0+width, y+y0+height)
@@ -72,46 +64,13 @@ func Capture(x, y, width, height int) (img *image.RGBA, e error) {
 	if !intersect.Empty() {
 		var data []byte
 
-		if useShm {
-			shmSize := intersect.Dx() * intersect.Dy() * 4
-			shmId, err := shm.Get(shm.IPC_PRIVATE, shmSize, shm.IPC_CREAT|0777)
-			if err != nil {
-				return nil, err
-			}
-
-			seg, err := mshm.NewSegId(c)
-			if err != nil {
-				return nil, err
-			}
-
-			data, err = shm.At(shmId, 0, 0)
-			if err != nil {
-				return nil, err
-			}
-
-			mshm.Attach(c, seg, uint32(shmId), false)
-
-			defer mshm.Detach(c, seg)
-			defer shm.Rm(shmId)
-			defer shm.Dt(data)
-
-			_, err = mshm.GetImage(c, xproto.Drawable(screen.Root),
-				int16(intersect.Min.X), int16(intersect.Min.Y),
-				uint16(intersect.Dx()), uint16(intersect.Dy()), 0xffffffff,
-				byte(xproto.ImageFormatZPixmap), seg, 0).Reply()
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			xImg, err := xproto.GetImage(c, xproto.ImageFormatZPixmap, xproto.Drawable(screen.Root),
-				int16(intersect.Min.X), int16(intersect.Min.Y),
-				uint16(intersect.Dx()), uint16(intersect.Dy()), 0xffffffff).Reply()
-			if err != nil {
-				return nil, err
-			}
-
-			data = xImg.Data
+		xImg, err2 := xproto.GetImage(c, xproto.ImageFormatZPixmap, xproto.Drawable(screen.Root),
+			int16(intersect.Min.X), int16(intersect.Min.Y),
+			uint16(intersect.Dx()), uint16(intersect.Dy()), 0xffffffff).Reply()
+		if err2 != nil {
+			return nil, err2
 		}
+		data = xImg.Data
 
 		// BitBlt by hand
 		offset := 0
